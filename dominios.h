423be7e4d02734d6025475a8410f8ff076d5c0a3
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-04-01 08:30:07 -0300

    Changes to be committed: 	modified:   dominios.h 	modified:   entidades.h

--- dominios.h
+++ dominios.h
@@ -2,86 +2,86 @@
 #define DOMINIOS_H_INCLUDED
 
 #include <stdexcept>
+#include <string>
 using namespace std;
-// CLASSE MATRICULA
-class matricula {
-   public:
-    void set_matricula(int matricula);
-    int get_matricula();
 
+// CLASS CLASSE
+class classe {
    private:
-    int matricula;
-};
-
-// CLASSE SENHA
-class senha {
    public:
-    void set_senha(string senha);
-    string get_senha();
-
-   private:
-    string senha;
-};
-
-// CLASSE TELEFONE
-class telefone {
-   public:
-    void set_telefone(int telefone);
-    int get_telefone();
-
-   private:
-    int telefone;
-};
-
-// CLASSE RESULTADO
-class resultado {
-   public:
-    string get_resultado();
-
-   private:
-    resultado();
-
-};
-
-// CLASSE DATA
-class data {
-   public:
-    void set_data(string data);
-    string get_data();
-
-   private:
-    string data;
 };
 
 // CLASSE CODIGO
 class codigo {
+   private:
+    string codigo;
+
    public:
     void set_codigo(string codigo);
     string get_codigo();
-
-   private:
-    string codigo;
 };
 
-// CLASS CLASSE
-class classe {
-   public:
+// CLASSE DATA
+class data {
    private:
+    string data;
+
+   public:
+    void set_data(string data);
+    string get_data();
+};
+
+// CLASSE MATRICULA
+class matricula {
+   private:
+    int matricula;
+
+   public:
+    void set_matricula(int matricula);
+    int get_matricula();
+};
+
+// CLASSE RESULTADO
+class resultado {
+   private:
+    resultado();
+
+   public:
+    string get_resultado();
+};
+
+// CLASSE SENHA
+class senha {
+   private:
+    string senha;
+
+   public:
+    void set_senha(string senha);
+    string get_senha();
+};
+
+// CLASSE TELEFONE
+class telefone {
+   private:
+    int telefone;
+
+   public:
+    void set_telefone(int telefone);
+    int get_telefone();
 };
 
 // CLASSE TEXTO
 class texto {
+   private:
+    string texto;
+
    public:
     void set_texto(string texto);
     string get_texto();
-
-   private:
-    string texto;
 };
 
 // IMPLEMENTAÇÃO DA MATRÍCULA
-void
-matricula::set_matricula(int matricula) {
+void matricula::set_matricula(int matricula) {
     this->matricula = matricula;
 }
 
---
+++ entidades.h
@@ -0,0 +1,23 @@
+#ifndef ENTIDADES_H_INCLUDED
+#define ENTIDADES_H_INCLUDED
+
+class dev{
+    private:
+
+    public:
+};
+
+class teste{
+    private:
+
+    public:
+};
+
+class testCase{
+    private:
+
+    public:
+};
+
+
+#endif // ENTIDADES_H_INCLUDED
