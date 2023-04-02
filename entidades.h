5ba557379c2ceb6a5bddef6c1f2ebcc7263f2eeb
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-04-02 18:41:33 -0300

    Changes to be committed: 	modified:   entidades.h

--- entidades.h
+++ entidades.h
@@ -2,38 +2,39 @@
 #define ENTIDADES_H_INCLUDED
 
 #include "dominios.h"
-class desenvolvedor{
-    private:
-        desenvolvedor(){
-            Matricula =  new matricula();
-            Texto = new texto();
-            Senha = new senha();
-            Telefone = new telefone();
-        }
+class desenvolvedor {
+   private:
+    desenvolvedor() {
+        Matricula *matricula = new Matricula();
+        Texto *texto = new Texto();
+        Senha *senha = new Senha();
+        Telefone *telefone = new Telefone();
+    }
 
-    public:
+   public:
 };
 
-class teste{
-    private:
-        teste(){
-            Nome = new nome();
-            Classe = new classe();
-        }
-    public:
+class teste {
+   private:
+    teste() {
+        Nome = new nome();
+        Classe = new classe();
+    }
+
+   public:
 };
 
-class caso_teste{
-    private:
-        caso_teste(){
-            Nome = new nome();
-            Data = new data();
-            Acao = new acao();
-            Resposta = new resposta();
-            Resultado = new resultado();
-        }
-    public:
+class caso_teste {
+   private:
+    caso_teste() {
+        Nome = new nome();
+        Data = new data();
+        Acao = new acao();
+        Resposta = new resposta();
+        Resultado = new resultado();
+    }
+
+   public:
 };
 
-
-#endif // ENTIDADES_H_INCLUDED
+#endif  // ENTIDADES_H_INCLUDED
