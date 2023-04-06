cdb268c85a064dfc043660cea74da188fd531144
jpantonow <129221613+jpantonow@users.noreply.github.com>
2023-04-06 10:34:33 -0300

    ok

--- headers/entidades.h
+++ headers/entidades.h
@@ -2,17 +2,61 @@
 #define ENTIDADES_H_INCLUDED
 
 #include "dominios.h"
-class desenvolvedor {
+#include <string>
+using namespace std;
+
+class Desenvolvedor {
    private:
-    desenvolvedor() {
-        Matricula *matricula = new Matricula();
-        Texto *texto = new Texto();
-        Senha *senha = new Senha();
-        Telefone *telefone = new Telefone();
-    }
+    Matricula matricula;
+    Texto nome;
+    Telefone telefone;
+    Senha senha;
 
    public:
+    void setMatricula(const Matricula&);
+    Matricula getMatricula() const;
+    void setTexto(const Texto&) const;
+    Texto getTexto() const;
+    void setTelefone(const Telefone&);
+    Telefone getTelefone() const;
+    void setSenha(const Senha&) const;
+    Senha getSenha() const;
 };
+//IMPLEMENTAÇÃO DOS MÉTODOS INLINE
+
+//Matricula
+//OBS: implementar o <PK> protected key
+inline void Desenvolvedor::set_Matricula(const Matricula&){
+    this-> matricula = matricula;
+}
+inline Matricula Desenvolvedor::getMatricula() const{
+    return matricula;
+}
+
+//Nome
+inline void Desenvolvedor::set_Texto(const Texto&) const{
+    this->texto = nome;
+}
+inline Texto Desenvolvedor::get_Texto() const{
+    return nome;
+}
+
+//Telefone
+inline void Desenvolvedor::set_Telefone(const Telefone&){
+    this-> telefone = telefone;
+}
+inline Telefone Desenvolvedor::get_Telefone() const{
+    return telefone;
+}
+
+//Senha
+inline void Desenvolvedor::set_Senha(const Senha&){
+    this->senha = senha;
+}
+inline Senha Desenvolvedor::get_Senha() const{
+    return senha;
+}
+
 
 class teste {
    private:
