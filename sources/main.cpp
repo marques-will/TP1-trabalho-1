4421c86f84670f7d157a98c138276325a0fbd779
jpantonow <jpantonow@gmail.com>
2023-04-11 14:23:29 -0300

    ok

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "../headers/dominios.h"
 #include "dominios.cpp"
+#include <locale.h>
 //#include "../headers/entidades.h"
 //#include "../headers/testes_dominios.h"
 //#include "../headers/testes_entidades.h"
@@ -8,8 +9,9 @@
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
     Codigo teste;
-    teste.setValor("abc123");
+    teste.setValor("ab123");
     cout << teste.getValor();
     return 0;
 }
