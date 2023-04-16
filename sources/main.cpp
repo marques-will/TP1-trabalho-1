40f4fa785198f0dc0c559744fc87dd8dcca6d205
jpantonow <129221613+jpantonow@users.noreply.github.com>
2023-04-16 08:36:24 -0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,36 +1,28 @@
-#include <locale.h>
-
 #include <iostream>
 
 #include "../headers/dominios.h"
-#include "dominios.cpp"
-// #include "../headers/entidades.h"
-// #include "../headers/testes_dominios.h"
-// #include "../headers/testes_entidades.h"
-
+#include "../headers/entidades.h"
+#include "../headers/testes_dominios.h"
+#include "../headers/testes_entidades.h"
+#include "testes_dominios.cpp"
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
-    Codigo teste;
-    teste.setValor("abc123");
-    cout << teste.getValor() << endl;
+    TUTelefone teste;
+    int resultado = teste.run();
+    if(resultado == 0){
+        cout << "deu certo" << endl;
+    }
+    if(resultado ==-1){
+        cout <<"deu errado" << endl;
+    }
+    //switch (teste.run()) {
+      //  case TUTelefone::SUCESSO:
+        //    cout << "Dominios::Telefone - teste bem-sucedido" << endl;
+          //  break;
 
-    Telefone telefone;
-    telefone.setValor("+12345678");
-    cout << telefone.getValor() << endl;
-
-    Matricula matricula;
-    matricula.setValor("1234567");
-    cout << matricula.getValor() << endl;
-
-    Texto texto;
-    texto.setValor("o joao eh broxa");
-    cout << texto.getValor() << endl;
-
-    Data data;
-    data.setValor("29/FEV/2021");
-    cout << data.getValor() << endl;
-
+      //  case TUTelefone::FALHA:
+        //    cout << "Dominios::Telefone - teste falhou" << endl;
+          //  break;
     return 0;
-}
+    }
