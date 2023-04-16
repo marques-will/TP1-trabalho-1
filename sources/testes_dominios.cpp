484c91acad7748c13fb3a1eda5d722dd972e00b6
jpantonow <jpantonow@gmail.com>
2023-04-16 07:14:18 -0300

    ok

--- sources/testes_dominios.cpp
+++ sources/testes_dominios.cpp
@@ -1,43 +1,52 @@
-// #include "../headers/testes_dominios.h"
+#include "../headers/testes_dominios.h"
+#include "../headers/dominios.h"
+#include "../sources/dominios.cpp"
+using namespace std;
+
 
 // //IMPLEMENTAÇÃO DO TESTE DO TELEFONE
-// void TUTelefone::setUp(){
-//     telefone = new Telefone();
-//     estado = sucesso;
-// }
+const int TUTelefone::SUCESSO;
+const int TUTelefone::FALHA;
+string TUTelefone::VALOR_VALIDO;
+string TUTelefone::VALOR_INVALIDO;
 
-// void TUTelefone::tearDown(){
-//     delete telefone;
-// }
+ void TUTelefone::setUp(){
+     telefone = new Telefone();
+     estado = SUCESSO;
+ }
 
-// void TUTelefone::testarCenariosucesso(){
-//     try{
-//         telefone->setValor(valido);
-//         if (telefone->getValor() != valido)
-//             estado = falha;
-//     }
-//     catch(invalid_argument &excecao){
-//         estado = falha;
-//     }
-// }
+ void TUTelefone::tearDown(){
+     delete telefone;
+ }
 
-// void TUTelefone::testarCenariofalha(){
-//     try{
-//         telefone->setValor(invalido);
-//         estado = falha;
-//     }
-//     catch(invalid_argument &excecao){
-//         if (telefone->getValor() == invalido)
-//             estado = falha;
-//     }
-// }
+ void TUTelefone::testarCenarioSucesso(){
+     try{
+         telefone->setValor(VALOR_VALIDO);
+         if (telefone->getValor() != VALOR_VALIDO)
+             estado = FALHA;
+     }
+     catch(invalid_argument &excecao){
+         estado = FALHA;
+     }
+ }
 
-// int TUTelefone::run(){
-//     setUp();
-//     testarCenariosucesso();
-//     testarCenariofalha();
-//     tearDown();
-//     return estado;
-// }
+ void TUTelefone::testarCenarioFalha(){
+     try{
+         telefone->setValor(VALOR_INVALIDO);
+         estado = FALHA;
+     }
+     catch(invalid_argument &excecao){
+         if (telefone->getValor() == VALOR_INVALIDO)
+             estado = FALHA;
+     }
+ }
+
+ int TUTelefone::run(){
+     setUp();
+     testarCenarioSucesso();
+     testarCenarioFalha();
+     tearDown();
+     return estado;
+ }
 
 // //IMPLEMENTAÇÃO DO TESTE DA SENHA
