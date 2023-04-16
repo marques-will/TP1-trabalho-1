16262932ff51800bfc4c6e0cff394b279e1ac263
jpantonow <129221613+jpantonow@users.noreply.github.com>
2023-04-16 07:46:51 -0300

    Update testes_dominios.cpp

--- sources/testes_dominios.cpp
+++ sources/testes_dominios.cpp
@@ -1,52 +1,42 @@
 #include "../headers/testes_dominios.h"
 #include "../headers/dominios.h"
-#include "../sources/dominios.cpp"
-using namespace std;
+#include "dominios.cpp"
+// IMPLEMENTAÇÃO DO TESTE DO TELEFONE
+const string TUTelefone::VALOR_VALIDO = "+1234567";
+const string TUTelefone::VALOR_INVALIDO = "+1";
+void TUTelefone::setUp() {
+    telefone = new Telefone();
+    estado = SUCESSO;
+}
 
+void TUTelefone::tearDown() {
+    delete telefone;
+}
 
-// //IMPLEMENTAÇÃO DO TESTE DO TELEFONE
-const int TUTelefone::SUCESSO;
-const int TUTelefone::FALHA;
-string TUTelefone::VALOR_VALIDO;
-string TUTelefone::VALOR_INVALIDO;
+void TUTelefone::testarCenarioSucesso() {
+    try {
+        telefone->setValor(VALOR_VALIDO);
+        if (telefone->getValor() != VALOR_VALIDO)  // verifica funcionamente do setter e do getter
+            estado = FALHA;
+    } catch (invalid_argument &excecao) {  // verifica se o validador identificou como valor como válido
+        estado = FALHA;
+    }
+}
 
- void TUTelefone::setUp(){
-     telefone = new Telefone();
-     estado = SUCESSO;
- }
+void TUTelefone::testarCenarioFalha() {
+    try {
+        telefone->setValor(VALOR_INVALIDO);
+        estado = FALHA;  // verifica se o validador setou valor inválido
+    } catch (invalid_argument &excecao) {
+        if (telefone->getValor() == VALOR_INVALIDO)  // verifica o setter
+            estado = FALHA;
+    }
+}
 
- void TUTelefone::tearDown(){
-     delete telefone;
- }
-
- void TUTelefone::testarCenarioSucesso(){
-     try{
-         telefone->setValor(VALOR_VALIDO);
-         if (telefone->getValor() != VALOR_VALIDO)
-             estado = FALHA;
-     }
-     catch(invalid_argument &excecao){
-         estado = FALHA;
-     }
- }
-
- void TUTelefone::testarCenarioFalha(){
-     try{
-         telefone->setValor(VALOR_INVALIDO);
-         estado = FALHA;
-     }
-     catch(invalid_argument &excecao){
-         if (telefone->getValor() == VALOR_INVALIDO)
-             estado = FALHA;
-     }
- }
-
- int TUTelefone::run(){
-     setUp();
-     testarCenarioSucesso();
-     testarCenarioFalha();
-     tearDown();
-     return estado;
- }
-
-// //IMPLEMENTAÇÃO DO TESTE DA SENHA
+int TUTelefone::run() {
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
