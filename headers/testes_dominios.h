9db46470bf6860891285798a0f114db158f794c7
jpantonow <129221613+jpantonow@users.noreply.github.com>
2023-04-16 07:47:25 -0300

    Update testes_dominios.h

--- headers/testes_dominios.h
+++ headers/testes_dominios.h
@@ -1,31 +1,36 @@
+#ifndef TESTES_DOMINIOS_H_INCLUDED
+#define TESTES_DOMINIOS_H_INCLUDED
+
 #include <string>
+
 #include "../headers/dominios.h"
+
 using namespace std;
 
 // Matrícula
-class TUMatricula {
-   private:
-    string VALOR_VALIDO = "1234567";
-    string VALOR_INVALIDO = "1234569";
-    Matricula *matricula;
-    int estado;
+// class TUMatricula {
+//    private:
+//     constexpr static char VALOR_VALIDO[];
+//     constexpr static char VALOR_INVALIDO[];
+//     Matricula *matricula;
+//     int estado;
 
-    void setUp();
-    void tearDown();
-    void testarCenarioSucesso();
-    void testarCenarioFalha();
+//     void setUp();
+//     void tearDown();
+//     void testarCenarioSucesso();
+//     void testarCenarioFalha();
 
-   public:
-    const static int SUCESSO = 0;
-    const static int FALHA = -1;
-    int run();
-};
+//    public:
+//     constexpr static int SUCESSO = 0;
+//     constexpr static int FALHA = -1;
+//     int run();
+// };
 
 // Telefone
 class TUTelefone {
    private:
-    string VALOR_VALIDO = "+123456";
-    string VALOR_INVALIDO = "+1234567";
+    const static string VALOR_VALIDO;
+    const static string VALOR_INVALIDO;
     Telefone *telefone;
     int estado;
     void setUp();
@@ -34,61 +39,63 @@ class TUTelefone {
     void testarCenarioFalha();
 
    public:
-    const static int SUCESSO = 0;
-    const static int FALHA = -1;
+    constexpr static int SUCESSO = 0;
+    constexpr static int FALHA = -1;
     int run();
 };
 
 // Data
-class TUData {
-   private:
-    string VALOR_VALIDO = "10/JAN/2001";
-    string VALOR_INVALIDO = "10/JAN/1999";
-    Data *data;
-    int estado;
-    void setUp();
-    void tearDown();
-    void testarCenarioSucesso();
-    void testarCenarioFalha();
+// class TUData {
+//    private:
+//     constexpr static char VALOR_VALIDO[];
+//     constexpr static char VALOR_INVALIDO[];
+//     Data *data;
+//     int estado;
+//     void setUp();
+//     void tearDown();
+//     void testarCenarioSucesso();
+//     void testarCenarioFalha();
 
-   public:
-    const static int SUCESSO = 0;
-    const static int FALHA = -1;
-    int run();
-};
+//    public:
+//     constexpr static int SUCESSO = 0;
+//     constexpr static int FALHA = -1;
+//     int run();
+// };
 
-// Senha
-class TUSenha {
-   private:
-    //const static string VALOR_VALIDO;
-    //const static string VALOR_INVALIDO;
-    Senha *senha;
-    int estado;
-    void setUp();
-    void tearDown();
-    void testarCenarioSucesso();
-    void testarCenarioFalha();
+// // Senha
+// class TUSenha {
+//    private:
+//     constexpr static char VALOR_VALIDO[];
+//     constexpr static char VALOR_INVALIDO[];
+//     Senha *senha;
+//     int estado;
+//     void setUp();
+//     void tearDown();
+//     void testarCenarioSucesso();
+//     void testarCenarioFalha();
 
-   public:
-    const static int SUCESSO = 0;
-    const static int FALHA = -1;
-    int run();
-};
+//    public:
+//     constexpr static int SUCESSO = 0;
+//     constexpr static int FALHA = -1;
+//     int run();
+// };
 
-// Texto
-class TUTexto {
-   private:
-    //const static string VALOR_VALIDO;
-    //const static string VALOR_INVALIDO;
-    Texto *texto;
-    int estado;
-    void setUp();
-    void tearDown();
-    void testarCenarioSucesso();
-    void testarCenarioFalha();
+// // Texto
+// class TUTexto {
+//    private:
+//     constexpr static char VALOR_VALIDO[];
+//     constexpr static char VALOR_INVALIDO[];
+//     Texto *texto;
+//     int estado;
+//     void setUp();
+//     void tearDown();
+//     void testarCenarioSucesso();
+//     void testarCenarioFalha();
 
-   public:
-    const static int SUCESSO = 0;
-    const static int FALHA = -1;
-    int run();
-};
+//    public:
+//     constexpr static int SUCESSO = 0;
+//     constexpr static int FALHA = -1;
+//     int run();
+// };
+
+#endif  // TESTES_DOMINIOS_H_INCLUDED
