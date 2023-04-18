2f3c0e9aa6967fad8bdbf8da171d694b2d02cf6d
LuisFlexa <109433866+LuisFlexa@users.noreply.github.com>
2023-04-18 11:40:14 -0300

    Update dominios.cpp

--- sources/dominios.cpp
+++ sources/dominios.cpp
@@ -193,3 +193,19 @@ void Texto::validar(const string& texto) {
 void Resultado::validar(const string& resultado) {
     vector <string> retornos = {"APROVADO", "REPROVADO"}
     
+// VALIDAÇÃO DE CLASSE
+void Classe::validar(string& claasse) {
+    vector<string> classes = {};
+    bool encontrou = false;
+    for (const auto& classe: classes) {
+        if (classe == classes) {
+            encontrou = true;
+            break;
+        }
+    }
+    void if (encontrou == false) {
+        throw invalid_argument("Classe(s) inválida(s).");
+    } 
+       
+
+    
